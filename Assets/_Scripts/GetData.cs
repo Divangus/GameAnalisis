0727b447b71d02aeec762b5c0c58acbcf4efd3e4
David Benages <79161120+Divangus@users.noreply.github.com>
2023-10-04 12:09:55 +0200

    Get Data

--- Assets/_Scripts/GetData.cs
+++ Assets/_Scripts/GetData.cs
@@ -7,21 +7,45 @@ using UnityEngine.Networking;
 public class GetData : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    struct _user
+    {
+        string name;
+        string country;
+        DateTime dateTime;
+
+        public _user(string name, string country, DateTime dateTime)
+        {
+            this.name = name; 
+            this.country = country;
+            this.dateTime = dateTime;
+        }
+    }
+    private string _url;
+
     void OnEnable()
     {
         Simulator.OnNewPlayer += OnPLayerAdded;
     }
 
-    private void OnPLayerAdded(string arg1, string arg2, DateTime time)
+    private void OnDisable()
+    {
+        Simulator.OnNewPlayer -= OnPLayerAdded;
+    }
+
+    private void OnPLayerAdded(string name, string country, DateTime dateTime)
     {
         Debug.Log("on send player");
+
+        _user User = new _user(name, country, dateTime);
+
     }
 
 
 
     IEnumerator Upload()
     {
-        using (UnityWebRequest www = UnityWebRequest.Post("https://www.my-server.com/myapi", "{ \"field1\": 1, \"field2\": 2 }", "application/json"))
+        using (UnityWebRequest www = UnityWebRequest.Post(_url, "{ \"field1\": 1, \"field2\": 2 }", "application/json"))
         {
             yield return www.SendWebRequest();
 
