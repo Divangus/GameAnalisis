923f8ad5a25942b4fd015b7298d2795d570df66a
DarkAvanger <awesomekoala19@gmail.com>
2023-10-11 12:17:22 +0200

    PHP

---
+++ ServerPHP.php
@@ -0,0 +1,65 @@
+<?php
+// Database configuration
+$servername = "localhost";
+$username = "laiapp4";
+$password = "wEa6PS8XgQ7b";
+$database = "laiapp4";
+
+// Create a database connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Read the JSON data from the request body
+    $jsonData = $_POST['jsonData'];
+
+    // Decode the JSON data into an associative array
+    $userData = json_decode($jsonData, true);
+
+    if ($userData !== null) {
+        $name = $userData['name'];
+        $country = $userData['country'];
+        $gender = $userData['gender'];
+        $age = $userData['age'];
+        //$dateTime = $userData['dateTime'];
+
+        //echo $name;
+
+        // Prepare an SQL statement to insert the data into a table
+        $sql = "INSERT INTO Users (name, country, gender, age/*, datetime*/) VALUES (?, ?, ?, ?, ?)";
+        
+        // Create a prepared statement
+        $stmt = $conn->prepare($sql);
+
+        if ($stmt) {
+            // Bind the parameters
+        $stmt->bind_param("sssis", $name, $country, $gender, $age/*, $dateTime*/);
+
+            // Execute the statement
+            if ($stmt->execute()) {
+                echo "Data inserted into the database successfully.";
+            } else {
+                echo "Error inserting data: " . $stmt->error;
+            }
+
+            // Close the statement
+            $stmt->close();
+        } else {
+            echo "Error preparing statement: " . $conn->error;
+        }
+    } else {
+        echo "Error decoding JSON data.";
+        error_log("Received JSON data: " . $jsonData);
+    }
+} else {
+    echo "Invalid request method. Use POST to send data.";
+}
+
+// Close the database connection
+$conn->close();
+?>
